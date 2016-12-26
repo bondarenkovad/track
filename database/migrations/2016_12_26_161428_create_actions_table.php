92b015293386d1959e13b35c61e4ef994a4a0384
Vad Bondarenko <bondarenkowow@gmail.com>
2016-12-26 19:23:46 +0300

    #1 Add migration - Add action migrate to database

---
+++ database/migrations/2016_12_26_161428_create_actions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('method_key', 50)->unique();
+            $table->string('name', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('actions');
+    }
+}
