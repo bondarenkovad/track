458837bd97a5b579649b8a4d60a0ddad9ad047ee
Vad Bondarenko <bondarenkowow@gmail.com>
2017-01-05 14:04:46 +0300

    #1 Make migration - Add model Action has relation to Property Group model

---
+++ app/Action.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Property_Group;
+
+class Action extends Model
+{
+    public function property_groups()
+    {
+        return $this->hasMany('App/Property_Group');
+    }
+}
