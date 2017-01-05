2a66392c597f7e6375a2322be0adebf570f81e49
Vad Bondarenko <bondarenkowow@gmail.com>
2017-01-05 13:58:52 +0300

    #1 Make migration - Add model and migration to relationship between Property Group and Actions

--- app/Property_Group.php
+++ app/Property_Group.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Action;
 use Illuminate\Database\Eloquent\Model;
 
 class Property_Group extends Model
 {
-    //
+    public function actions()
+    {
+        return $this->hasMany('App/Action');
+    }
 }
--- database/migrations/2017_01_05_102741_create_property__groups_table.php
+++ database/migrations/2017_01_05_102741_create_property__groups_table.php
@@ -12,10 +12,29 @@ class CreatePropertyGroupsTable extends Migration
      */
     public function up()
     {
-        Schema::create('property__groups', function (Blueprint $table) {
+        Schema::create('property_groups', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name', 50);
             $table->timestamps();
         });
+
+        Schema::create('action_property_group', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('property_group_id')->unsigned();
+            $table->foreign('property_group_id')
+                ->references('id')
+                ->on('property_groups')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->integer('action_id')->unsigned();
+            $table->foreign('action_id')
+                ->references('id')
+                ->on('actions')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+
     }
 
     /**
