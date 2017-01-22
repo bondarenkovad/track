b200fdb48324b7adf4f8c2e89a74948f48b94bd9
Vad Bondarenko <bondarenkowow@gmail.com>
2017-01-22 14:46:49 +0300

    #5 - Modify user/edit.blade.php two field in password input and confirmation password - Modify update UserController method if we update password - insert new in database, if not - leave old

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,9 +79,7 @@ class UserController extends Controller
     {
         $allGroup = Group::all();
         $user=User::find($id);
-
         $groups = $request->group;
-        //dd(array_keys($groups));
 
         if($groups === null)
         {
@@ -116,115 +114,35 @@ class UserController extends Controller
                 }
         }
 
-//        dd($user->hasUserHaveGroupId(2));
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'active' => 'required|integer|between:0,1',
+            'email' => 'required|email|max:255',
+            'password' => 'sometimes|min:6|confirmed',
+        ]);
 
-//        foreach($allGroup as $group)
-//        {
-//            if($groups === null)
-//            {
-//
-//                print_r($user->hasUserHaveGroupId(1));
-////
-//            }
-//            if(array_key_exists($group->id, $groups))
-//            {
-//                $user->addGroupToUser($group->id);
-//            }
-//            else
-//            {
-//               $user->deleteGroupToUser($group->id);
-//
-//            }
-//        }
+        if($request->password === "")
+        {
+            $user->update([
+                [$user->name = $request->name],
+                [$user->active = $request->active],
+                [$user->email = $request->email],
+            ]);
+        }
+        else
+        {
+            $user->update([
+                'name' => $request['name'],
+                'email' => $request['email'],
+                'password' => bcrypt($request['password']),
+                'active'=>(int)$request['active'],
+            ]);
+        }
 
-//        $isAdmin = $user->hasGroup('Administrator');
-//        $isUser = $user->hasGroup('User');
-//        $isPM = $user->hasGroup('PM');
-//
-//      //  dd($isAdmin, $isUser, $isPM,$admin, $us, $pm);
-////        dd($request->Administrator);
-//
-//        //==========================
-//        if($isAdmin && ($admin == null))
-//        {
-//            $user->deleteGroupToUser('Administrator');
-//        }
-//
-//        if($isUser && ($us == null))
-//        {
-//            $user->deleteGroupToUser('User');
-//        }
-//
-//        if($isPM && ($pm == null))
-//        {
-//            $user->deleteGroupToUser('PM');
-//        }
-//
-//        if(!$admin == null)
-//        {
-//            if($user->hasGroup('Administrator'))
-//            {
-////                echo "Админ права уже есть!";
-//            }
-//            else
-//            {
-//                $user->addGroupToUser('Administrator');
-//            }
-//
-//        }
-//        else
-//        {
-////            echo "Aдмин чекбокс не выбран!";
-//        }
+        $user->save();
+        session()->flash('status', 'User successfully saved!');
 
-//         if(!$us == null)
-//         {
-//             if($user->hasGroup('User'))
-//             {
-////                 echo "Юзер права уже есть!";
-//             }
-//             else
-//             {
-//                 $user->addGroupToUser('User');
-//             }
-//         }
-//         else
-//         {
-////             echo "Юзер чекбокс не выбран!";
-//         }
-//
-//         if(!$pm == null)
-//         {
-//            if($user->hasGroup('PM'))
-//            {
-////                echo "PM права уже есть!";
-//            }
-//            else
-//            {
-//                $user->addGroupToUser('PM');
-//            }
-//         }
-//         else
-//         {
-////             echo "Пм чекбокс не выбран!";
-//         }
-
-        //dd($admin, $us, $pm);
-
-//        $user->addGroupToUser();
-
-
-
-//        $this->validate($request, [
-//            'name' => 'required|max:255',
-//            'active' => 'required|integer|between:0,1',
-//            'email' => 'required|email|max:255',
-//        ]);
-//        $user->update($request->all());
-//        $user->save();
-//        session()->flash('status', 'User successfully saved!');
         return redirect('user/index');
-
     }
 
 
