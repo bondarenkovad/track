aa50efe7d149ceaf107b13e02024a99f3933ce7a
Vad Bondarenko <bondarenkowow@gmail.com>
2017-02-13 18:54:11 +0300

    #24 - Add icon to  issue status and priority - Modify project/board view

--- resources/views/project/board.blade.php
+++ resources/views/project/board.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="panel-body">
                 <div class="form-group">
-                    <label for="active" class="col-md-4 control-label">Issues:</label>
+                    <label for="active" class="col-md-4 control-label">Backlog</label>
                     <div class="col-md-12">
                         <div class="row">
                             {{--<div id="left" class="form-control">--}}
@@ -16,13 +16,37 @@
                                     {{--@endforeach--}}
                                 {{--</ul>--}}
                             {{--</div>--}}
-                            <div>
                                 <ul id="sortable3" class="connectedSortable">
                                     @foreach($project->issues()->get() as $issue)
-                                        <li class="ui-state-default" data-value="{{$issue->id}}">{{$issue->summary}}</li>
+                                        <li class="ui-state-default" data-value="{{$issue->id}}">
+                                             <span class="imageSpan">
+                                                @if($issue->type['name'] === 'task')
+                                                    <img src="/img/status_icon/task.png" class="img">
+                                                @elseif($issue->type['name'] === 'story')
+                                                    <img src="/img/status_icon/story.png" class="img">
+                                                @elseif($issue->type['name'] === 'bug')
+                                                    <img src="/img/status_icon/bug.png" class="img">
+                                                @endif
+                                            </span>
+                                            <span class="summary">{{$issue->summary}}</span>
+                                            <span class="assign">{{$issue->assigned['name']}}</span>
+                                            <span class="key">{{$project->key}} - {{$issue->id}}</span>
+                                            <span class="prioritySpan">
+                                                 @if($issue->priority['name'] === 'trivial')
+                                                    <img src="/img/status_icon/trivial.png" class="img">
+                                                @elseif($issue->priority['name'] === 'minor')
+                                                    <img src="/img/status_icon/minor.png" class="img">
+                                                @elseif($issue->priority['name'] === 'major')
+                                                    <img src="/img/status_icon/major.png" class="img">
+                                                @elseif($issue->priority['name'] === 'critical')
+                                                    <img src="/img/status_icon/critical.png" class="img">
+                                                @elseif($issue->priority['name'] === 'blocker')
+                                                    <img src="/img/status_icon/blocker.png" class="img">
+                                                @endif
+                                            </span>
+                                        </li>
                                     @endforeach
                                 </ul>
-                            </div>
                         </div>
                         <input id="statusesId" type="hidden" name="statusesId">
                     </div>
