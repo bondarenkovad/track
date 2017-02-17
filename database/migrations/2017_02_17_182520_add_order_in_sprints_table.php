eadc9f4a9a84bc146f9ff9a0d806a7a73acf758b
Vad Bondarenko <bondarenkowow@gmail.com>
2017-02-17 21:27:38 +0300

    #26 - Add migration add string field order in sprints table

---
+++ database/migrations/2017_02_17_182520_add_order_in_sprints_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOrderInSprintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sprints', function (Blueprint $table) {
+            $table->string('order', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sprints', function (Blueprint $table) {
+            $table->dropColumn('order');
+
+        });
+    }
+}
