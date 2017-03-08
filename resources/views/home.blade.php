3246f437343f350b4fcf23a3bf27a326f580bf35
Vad Bondarenko <bondarenkowow@gmail.com>
2017-03-08 14:05:24 +0300

    #56 - Fix color icon by priority

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,11 +15,11 @@
                                 <li class="ui-state-default" data-value="{{$issue->id}}">
                                     <span class="imageSpan">
                                     @if($issue->type['name'] === 'task')
-                                        <span class="img glyphicon glyphicon-education" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
+                                        <span class="img glyphicon glyphicon-education low" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
                                         @elseif($issue->type['name'] === 'story')
-                                        <span class="img glyphicon glyphicon-file" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
+                                        <span class="img glyphicon glyphicon-file high" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
                                         @elseif($issue->type['name'] === 'bug')
-                                        <span class="img glyphicon glyphicon-fire" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
+                                        <span class="img glyphicon glyphicon-fire danger" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
                                     @endif
                                     </span>
                                     <span class="summary">{{$issue->summary}}</span>
@@ -44,19 +44,32 @@
                                     </span>
                                     <span class="prioritySpan">
                                         @if($issue->priority['name'] === 'trivial')
-                                            <span class="img glyphicon glyphicon-triangle-bottom" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
+                                            <span class="img glyphicon glyphicon-triangle-bottom low" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
                                         @elseif($issue->priority['name'] === 'minor')
                                             <span class="img glyphicon glyphicon-menu-down" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
                                         @elseif($issue->priority['name'] === 'major')
-                                            <span class="img glyphicon glyphicon-menu-up" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
+                                            <span class="img glyphicon glyphicon-menu-up high" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
                                         @elseif($issue->priority['name'] === 'critical')
-                                             <span class="img glyphicon glyphicon-alert" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
+                                             <span class="img glyphicon glyphicon-alert danger" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
                                         @elseif($issue->priority['name'] === 'blocker')
-                                            <span class="img glyphicon glyphicon-ban-circle" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
+                                            <span class="img glyphicon glyphicon-ban-circle danger" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
                                         @endif
                                     </span>
                                     <span>
                                     <span class="badge">{{date("H",$issue->original_estimate)}}</span>
+                                    </span>
+                                    <span class="statusColor">
+                                        @if($issue->status['name'] === 'open')
+                                            <span class="statusBGColorO">{{$issue->status['name']}}</span>
+                                        @elseif($issue->status['name'] === 'inProgress')
+                                            <span class="statusBGColorI">{{$issue->status['name']}}</span>
+                                        @elseif($issue->status['name'] === 'review')
+                                            <span class="statusBGColorR">{{$issue->status['name']}}</span>
+                                        @elseif($issue->status['name'] === 'testing')
+                                            <span class="statusBGColorT">{{$issue->status['name']}}</span>
+                                        @elseif($issue->status['name'] === 'done')
+                                            <span class="statusBGColorD">{{$issue->status['name']}}</span>
+                                        @endif
                                     </span>
                                     <span class="linkIssue">
                                         <a href="/project/{{$project->key}}/issue/{{$issue->id}}/view">View Issue</a>
