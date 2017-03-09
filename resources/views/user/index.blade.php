dd0e319fe221d54626b57e6e1a5b3658e95ff324
Vad Bondarenko <bondarenkowow@gmail.com>
2017-03-09 17:51:51 +0300

    #69 - Modify style links and couple details to users/index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,68 +2,66 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1"><h1 class="text-center text-muted">Users List</h1></div>
-            <div class="col-md-3">
-                <form action="/user/index" method="post">
-                    {{ csrf_field() }}
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for..." name="search">
-                     <span class="input-group-btn">
-                     <button class="btn btn-success" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                    </span>
-                </div>
-                </form>
+        <h4 class="text-left text-muted"><span class="userName">Users</span> List</h4>
+        <form action="/user/index" method="post">
+        {{ csrf_field() }}
+            <div class="input-group col-md-4">
+            <input type="text" class="form-control span2" placeholder="Search user..." name="search">
+            <span class="input-group-btn">
+            <button class="btn btn-success" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            </span>
             </div>
-            <div class="col-md-10 col-md-offset-10"><a  href="/user/add" class="btn btn-success">User Create</a></div>
-        </div>
-        <div class="row">
-            <table class="table table-hover">
-                <thead>
+        </form>
+        <div class="text-right" style="margin-right: 20px"><a  href="/user/add">User Create</a></div>
+        <table class="table table-hover">
+            <thead>
                 <th>Name</th>
                 <th>Photo</th>
                 <th>E-mail</th>
                 <th>Active</th>
                 <th>Groups</th>
                 <th>Projects</th>
-                <th>Show</th>
-                <th>Edit</th>
-                </thead>
-                <tbody>
+                <th></th>
+                <th></th>
+            </thead>
+            <tbody>
                 @foreach($users as $user)
-                    <tr>
-                            <td>{{$user->name}}</td>
-                            <td>
-                                @if($user->image_path != null)
-                                <img class="img img-circle" src="{{$user->image_path}}" data-toggle="tooltip" title="{{$user->name}}">
-                                    @else
-                                    <img class="img" src="/img/userPhoto/defaultPhoto.png" data-toggle="tooltip" title="{{$user->name}}">
-                                    @endif
-                            </td>
-                            <td>{{$user->email}} <input type="hidden" name="email" value="{{$user->email}}"></td>
-                            <td>
-                                @if($user->active)
-                                    <img src="/img/status_icon/on.png">
-                                @else
-                                    <img src="/img/status_icon/off.png">
-                                @endif
-                            </td>
-                            <td>
-                                <ul> @foreach($user->groups()->get() as $group)</ul>
-                                <li>{{$group->name}}</li>
-                                @endforeach
-                            </td>
-                            <td>
-                                <ul> @foreach($user->projects()->get() as $project)</ul>
-                                <li>{{$project->name}}</li>
-                                @endforeach
-                            </td>
-                            <td><a  href="/user/show/{{$user->id}}" class="btn btn-default" >Show</a> </td>
-                            <td><a  href="/user/edit/{{$user->id}}" class="btn btn-success" >Edit</a></td>
-                    </tr>
+                <tr>
+                <td>{{$user->name}}</td>
+                <td>
+                    @if($user->image_path != null)
+                    <img class="img img-circle" src="{{$user->image_path}}" data-toggle="tooltip" title="{{$user->name}}">
+                    @else
+                    <img class="img" src="/img/userPhoto/defaultPhoto.png" data-toggle="tooltip" title="{{$user->name}}">
+                    @endif
+                </td>
+                <td>{{$user->email}} <input type="hidden" name="email" value="{{$user->email}}"></td>
+                <td>
+                    @if($user->active)
+                    <img src="/img/status_icon/on.png">
+                    @else
+                    <img src="/img/status_icon/off.png">
+                    @endif
+                </td>
+                <td>
+                    <ul>
+                        @foreach($user->groups()->get() as $group)
+                            <li>{{$group->name}}</li>
+                        @endforeach
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        @foreach($user->projects()->get() as $project)
+                            <li>{{$project->name}}</li>
+                        @endforeach
+                    </ul>
+                </td>
+                <td><a  href="/user/show/{{$user->id}}">Show</a> </td>
+                <td><a  href="/user/edit/{{$user->id}}">Edit</a></td>
+                </tr>
                 @endforeach
-                </tbody>
-            </table>
-        </div>
+            </tbody>
+        </table>
     </div>
 @endsection
