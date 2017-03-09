474ab5e42f21c065e6a69dc2aec5ca79357251d9
Vad Bondarenko <bondarenkowow@gmail.com>
2017-03-09 17:16:02 +0300

    #66 - Modify view backlog

--- resources/views/project/backlog.blade.php
+++ resources/views/project/backlog.blade.php
@@ -10,12 +10,11 @@
                 {{ csrf_field() }}
                 @if($project->hasSprints())
                     @foreach($project->getSprints() as $sprint)
-                        <div>
-                            {{--@if($sprint->isActiveSprint())--}}
+                            @if($sprint->isActiveSprint())
                                 <a href="/project/{{$project->key}}/board/{{$board->id}}/sprint/{{$sprint->id}}">
                                 <span class="glyphicon glyphicon-hand-left"></span>
                                 </a>
-                            {{--@endif--}}
+                            @endif
                             <label>Sprint - <span class="userName">{{$sprint->id}}</span></label>
                             @if($sprint->order != null)
                                 <input id="issueData-{{$sprint->id}}" type="hidden" name="issueData[{{$sprint->id}}]" value="{{implode(',',json_decode($sprint->order))}}">
@@ -23,76 +22,77 @@
                                 <input id="issueData-{{$sprint->id}}" type="hidden" name="issueData[{{$sprint->id}}]">
                             @endif
                             <ul id="sprint-{{$sprint->id}}" class="connectedSortable sprintContainer" data-value="{{$sprint->id}}">
-                            @foreach($sprint->getIssueForSprint() as $issue)
-                            <li class="ui-state-default" data-value="{{$issue->id}}">
-                            <span class="imageSpan">
-                            @if($issue->type['name'] === 'task')
-                             <span class="img glyphicon glyphicon-education low" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
-                            @elseif($issue->type['name'] === 'story')
-                             <span class="img glyphicon glyphicon-file high" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
-                            @elseif($issue->type['name'] === 'bug')
-                             <span class="img glyphicon glyphicon-fire danger" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
-                            @endif
-                            </span>
-                            <span class="summary">{{$issue->summary}}</span>
-                            <span class="description">{{$issue->description}}</span>
-                            <span class="assign">
-                            <span>
-                            R:
-                            @if($issue->reporter['image_path'] != null)
-                            <img src="{{$issue->reporter['image_path']}}" class="img img-circle" data-toggle="tooltip" title="{{$issue->reporter['name']}}">
-                            @else
-                            <img src="/img/userPhoto/defaultPhoto.png" class="img img-circle" data-toggle="tooltip" title="{{$issue->reporter['name']}}">
-                            @endif
-                            </span>
-                            <span>
-                            A:
-                            @if($issue->assigned['image_path'] != null)
-                            <img src="{{$issue->assigned['image_path']}}" class="img img-circle" data-toggle="tooltip" title="{{$issue->assigned['name']}}">
-                            @else
-                            <img src="/img/userPhoto/defaultPhoto.png" class="img img-circle" data-toggle="tooltip" title="{{$issue->assigned['name']}}">
-                            @endif
-                            </span>
-                            </span>
-                            <span class="prioritySpan">
-                            @if($issue->priority['name'] === 'trivial')
-                            <span class="img glyphicon glyphicon-triangle-bottom low" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
-                            @elseif($issue->priority['name'] === 'minor')
-                            <span class="img glyphicon glyphicon-menu-down" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
-                            @elseif($issue->priority['name'] === 'major')
-                            <span class="img glyphicon glyphicon-menu-up high" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
-                            @elseif($issue->priority['name'] === 'critical')
-                            <span class="img glyphicon glyphicon-alert danger" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
-                            @elseif($issue->priority['name'] === 'blocker')
-                            <span class="img glyphicon glyphicon-ban-circle danger" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
-                            @endif
-                            </span>
-                            <span>
-                            <span class="badge">{{date("H",$issue->original_estimate)}}</span>
-                            </span>
-                            <span class="statusColor">
-                            @if($issue->status['name'] === 'open')
-                            <span class="statusBGColorO">{{$issue->status['name']}}</span>
-                            @elseif($issue->status['name'] === 'inProgress')
-                            <span class="statusBGColorI">{{$issue->status['name']}}</span>
-                            @elseif($issue->status['name'] === 'review')
-                            <span class="statusBGColorR">{{$issue->status['name']}}</span>
-                            @elseif($issue->status['name'] === 'testing')
-                            <span class="statusBGColorT">{{$issue->status['name']}}</span>
-                            @elseif($issue->status['name'] === 'done')
-                            <span class="statusBGColorD">{{$issue->status['name']}}</span>
-                            @endif
-                            </span>
-                            <span class="linkIssue">
-                            <a href="/project/{{$project->key}}/issue/{{$issue->id}}/view">View Issue</a>
-                            </span>
-                            </li>
-                            @endforeach
+                                @foreach($sprint->getIssueForSprint() as $issue)
+                                    <li class="ui-state-default" data-value="{{$issue->id}}">
+                                        <span class="imageSpan">
+                                        @if($issue->type['name'] === 'task')
+                                         <span class="img glyphicon glyphicon-education low" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
+                                        @elseif($issue->type['name'] === 'story')
+                                         <span class="img glyphicon glyphicon-file high" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
+                                        @elseif($issue->type['name'] === 'bug')
+                                         <span class="img glyphicon glyphicon-fire danger" data-toggle="tooltip" title="{{$issue->type['name']}}"></span>
+                                        @endif
+                                        </span>
+                                        <span class="summary">{{$issue->summary}}</span>
+                                        <span class="description">{{$issue->description}}</span>
+                                        <span class="assign">
+                                        <span>
+                                        R:
+                                        @if($issue->reporter['image_path'] != null)
+                                        <img src="{{$issue->reporter['image_path']}}" class="img img-circle" data-toggle="tooltip" title="{{$issue->reporter['name']}}">
+                                        @else
+                                        <img src="/img/userPhoto/defaultPhoto.png" class="img img-circle" data-toggle="tooltip" title="{{$issue->reporter['name']}}">
+                                        @endif
+                                        </span>
+                                        <span>
+                                        A:
+                                        @if($issue->assigned['image_path'] != null)
+                                        <img src="{{$issue->assigned['image_path']}}" class="img img-circle" data-toggle="tooltip" title="{{$issue->assigned['name']}}">
+                                        @else
+                                        <img src="/img/userPhoto/defaultPhoto.png" class="img img-circle" data-toggle="tooltip" title="{{$issue->assigned['name']}}">
+                                        @endif
+                                        </span>
+                                        </span>
+                                        <span class="prioritySpan">
+                                        @if($issue->priority['name'] === 'trivial')
+                                        <span class="img glyphicon glyphicon-triangle-bottom low" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
+                                        @elseif($issue->priority['name'] === 'minor')
+                                        <span class="img glyphicon glyphicon-menu-down" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
+                                        @elseif($issue->priority['name'] === 'major')
+                                        <span class="img glyphicon glyphicon-menu-up high" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
+                                        @elseif($issue->priority['name'] === 'critical')
+                                        <span class="img glyphicon glyphicon-alert danger" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
+                                        @elseif($issue->priority['name'] === 'blocker')
+                                        <span class="img glyphicon glyphicon-ban-circle danger" data-toggle="tooltip" title="{{$issue->priority['name']}}"></span>
+                                        @endif
+                                        </span>
+                                        <span>
+                                        <span class="badge">{{date("H",$issue->original_estimate)}}</span>
+                                        </span>
+                                        <span class="statusColor">
+                                        @if($issue->status['name'] === 'open')
+                                        <span class="statusBGColorO">{{$issue->status['name']}}</span>
+                                        @elseif($issue->status['name'] === 'inProgress')
+                                        <span class="statusBGColorI">{{$issue->status['name']}}</span>
+                                        @elseif($issue->status['name'] === 'review')
+                                        <span class="statusBGColorR">{{$issue->status['name']}}</span>
+                                        @elseif($issue->status['name'] === 'testing')
+                                        <span class="statusBGColorT">{{$issue->status['name']}}</span>
+                                        @elseif($issue->status['name'] === 'done')
+                                        <span class="statusBGColorD">{{$issue->status['name']}}</span>
+                                        @endif
+                                        </span>
+                                        <span class="linkIssue">
+                                        <a href="/project/{{$project->key}}/issue/{{$issue->id}}/view">View Issue</a>
+                                        </span>
+                                    </li>
+                                @endforeach
                             </ul>
-                        </div>
+                            <hr>
                     @endforeach
                 @endif
                         <div>
+                            <hr>
                             <label>Backlog</label>
                             @if(Auth::user()->ifAdmin() || Auth::user()->ifPM())
                             <a href="/sprint/add/{{$project->key}}">Create Sprint</a>
