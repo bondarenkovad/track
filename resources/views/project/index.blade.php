b139781cc323add842be3d58b0ef9b8244c97459
Vad Bondarenko <bondarenkowow@gmail.com>
2017-03-11 15:31:54 +0300

    #86 - Fixed style!

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -2,28 +2,22 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1"><h1 class="text-center text-muted">Projects List</h1></div>
-            <div class="col-md-3">
+        <h4 class="text-left text-muted"><span class="userName">Projects</span> List</h4>
                 <form action="/project/index" method="post">
                     {{ csrf_field() }}
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search for..." name="search">
+                    <div class="input-group col-md-4">
+                        <input type="text" class="form-control" placeholder="Search project..." name="search">
                      <span class="input-group-btn">
                      <button class="btn btn-success" type="submit"><i class="glyphicon glyphicon-search"></i></button>
                     </span>
                     </div>
                 </form>
-            </div>
-            <div class="col-md-10 col-md-offset-10"><a  href="/project/add" class="btn btn-success">Project Create</a></div>
-        </div>
-        <div class="row">
+        <div class="text-right"><a  href="/project/add">Project Create</a></div>
             <table class="table table-hover">
                 <thead>
                 <th>Name</th>
                 <th>Short key</th>
                 <th>Users</th>
-                <th></th>
                 <th></th>
                 <th></th>
                 </thead>
@@ -33,23 +27,23 @@
                         <td>{{$project->name}}</td>
                         <td>{{$project->key}}</td>
                         <td>
-                            <ul> @foreach($project->users()->get() as $user)</ul>
-                            <li>
-                                @if($user->image_path != null)
-                                    <img src="{{$user->image_path}}" class="img img-circle" data-toggle="tooltip" title="{{$user->name}}">
-                                @else
-                                    <img src="/img/userPhoto/defaultPhoto.png" class="img img-circle" data-toggle="tooltip" title="{{$user->name}}">
-                                @endif
-                            </li>
-                            @endforeach
+                            <ul>
+                                @foreach($project->users()->get() as $user)
+                                    <li>
+                                        @if($user->image_path != null)
+                                            <img src="{{$user->image_path}}" class="img img-circle" data-toggle="tooltip" title="{{$user->name}}">
+                                        @else
+                                            <img src="/img/userPhoto/defaultPhoto.png" class="img img-circle" data-toggle="tooltip" title="{{$user->name}}">
+                                        @endif
+                                    </li>
+                                @endforeach
+                            </ul>
                         </td>
-                        <td><a  href="/project/{{$project->key}}/backlog" class="btn btn-primary" >Show Backlog</a></td>
-                        <td><a  href="/project/edit/{{$project->id}}" class="btn btn-success" >Edit</a></td>
-                        <td><a  href="/project/delete/{{$project->id}}" class="btn btn-danger" >Delete</a></td>
+                        <td><a  href="/project/edit/{{$project->id}}" class="floatRight"><i class="glyphicon glyphicon-pencil" data-toggle="tooltip" title="Edit"></i></a></td>
+                        <td><a  href="/project/delete/{{$project->id}}" class="floatRight"><i class="glyphicon glyphicon-remove danger" data-toggle="tooltip" title="Delete"></i></a></td>
                     </tr>
                 @endforeach
                 </tbody>
             </table>
-        </div>
     </div>
 @endsection
