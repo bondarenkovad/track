6a039f2891b86c84fed26fd876ecd13d2e768ad9
Vad Bondarenko <bondarenkowow@gmail.com>
2017-05-13 16:42:35 +0300

    #166 - Modify IssueStatusController: - Replace database request to methods(getStatuses, etc.)

--- app/Http/Controllers/IssueStatusController.php
+++ app/Http/Controllers/IssueStatusController.php
@@ -15,9 +15,9 @@ class IssueStatusController extends Controller
         $this->middleware('auth');
     }
 
-    public function index(Request $request)
+    public function index()
     {
-        $issuesStatuses = IssueStatus::all();
+        $issuesStatuses = $this->getStatuses();
         return view('issue.status.index', ['issuesStatuses' => $issuesStatuses]);
     }
 
@@ -48,13 +48,13 @@ class IssueStatusController extends Controller
 
     public function edit($id)
     {
-        $issueStatus = IssueStatus::find($id);
+        $issueStatus = $this->getStatusById($id);
         return view('issue.status.edit', ['issueStatus' => $issueStatus]);
     }
 
     public function update($id, Request $request)
     {
-        $issueStatus = IssueStatus::find($id);
+        $issueStatus = $this->getStatusById($id);
 
         $this->validate($request, [
             'name' => 'required|max:50',
@@ -69,5 +69,15 @@ class IssueStatusController extends Controller
         session()->flash('status', 'Issue Status successfully updated!');
 
         return redirect('issue/status/index');
+    }
+
+    public function getStatuses()
+    {
+        return IssueStatus::all();
+    }
+
+    public function getStatusById($id)
+    {
+        return IssueStatus::find($id);
     }
 }
