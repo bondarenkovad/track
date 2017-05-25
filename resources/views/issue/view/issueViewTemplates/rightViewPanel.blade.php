dccc80ff07127e0bc62c477e99e63d3103265e23
Vad Bondarenko <bondarenkowow@gmail.com>
2017-05-25 21:09:08 +0300

    #168 - Modify style for desktop, mobile and laptop issue/view page

--- resources/views/issue/view/issueViewTemplates/rightViewPanel.blade.php
+++ resources/views/issue/view/issueViewTemplates/rightViewPanel.blade.php
@@ -1,21 +1,20 @@
-<div class="rightDivPeople">
+<div>
     <div class="strike">
         <span class="bolder">People</span>
     </div>
-    <div class="row">
         <dl class="dl-horizontal">
             <dt>Assignee:</dt>
             <dd>
-                            <span class="marginLeft">
-                                <a href="/user/show/{{$issue->assigned['id']}}" style="text-decoration: none">
-                                    @if($issue->assigned['image_path'] != null)
-                                        <img src="{{$issue->assigned['image_path']}}" class="img img-circle" data-toggle="tooltip" title="{{$issue->assigned['name']}}">
-                                    @else
-                                        <img src="/img/userPhoto/defaultPhoto.png" class="img img-circle" data-toggle="tooltip" title="{{$issue->assigned['name']}}">
-                                    @endif
-                                    {{$issue->assigned['name']}}
-                                </a>
-                            </span>
+                <span class="marginLeft">
+                    <a href="/user/show/{{$issue->assigned['id']}}" style="text-decoration: none">
+                        @if($issue->assigned['image_path'] != null)
+                            <img src="{{$issue->assigned['image_path']}}" class="img img-circle" data-toggle="tooltip" title="{{$issue->assigned['name']}}">
+                        @else
+                            <img src="/img/userPhoto/defaultPhoto.png" class="img img-circle" data-toggle="tooltip" title="{{$issue->assigned['name']}}">
+                        @endif
+                        {{$issue->assigned['name']}}
+                    </a>
+                </span>
             </dd>
             <dt>Reporter:</dt>
             <dd>
@@ -33,52 +32,53 @@
             <dt>&nbsp;</dt>
             <dd>&nbsp;</dd>
         </dl>
-    </div>
     <div class="strike">
         <span class="bolder">Dates</span>
     </div>
-    <div class="row">
         <dl class="dl-horizontal">
             <dt>Created:</dt>
             <dd>
-                            <span class="marginLeft">
-                                {{date("M j, Y, g:i a",strtotime($issue->created_at))}}
-                            </span>
+                <span class="marginLeft">
+                    {{date("M j, Y, g:i a",strtotime($issue->created_at))}}
+                </span>
             </dd>
             <dt>Updated:</dt>
             <dd>
-                             <span class="marginLeft">
-                                {{date("M j, Y, g:i a",strtotime($issue->updated_at))}}
-                            </span>
+                 <span class="marginLeft">
+                    {{date("M j, Y, g:i a",strtotime($issue->updated_at))}}
+                </span>
             </dd>
             <dt>Original Estimate:</dt>
             <dd>
-                            <span class="marginLeft">
-                                {{$issue->original_estimate}}h
-                            </span>
+                <span class="marginLeft">
+                    {{$issue->original_estimate}}h
+                </span>
             </dd>
             <dt>Remaining Estimate:</dt>
             <dd>
-                             <span class="marginLeft">
-                                 @if($issue->calcRE() < 0)
-                                     <span class="danger">{{$issue->calcRE()}}h</span>
-                                 @else
-                                     <span class="low">{{$issue->calcRE()}}h</span>
-                                 @endif
-                            </span>
+                 <span class="marginLeft">
+                     @if($issue->calcRE() < 0)
+                         <span class="danger">{{$issue->calcRE()}}h</span>
+                     @else
+                         <span class="low">{{$issue->calcRE()}}h</span>
+                     @endif
+                </span>
             </dd>
             <dt>Time Spent:</dt>
             <dd>
                 @if($issue->TimeSpentSum()!= null)
                     <span class="marginLeft">
-                                {{$issue->TimeSpentSum()}}h
-                            </span>
+                        {{$issue->TimeSpentSum()}}h
+                    </span>
                 @else
                     <span class="marginLeft">
-                                0
-                            </span>
+                        0
+                    </span>
                 @endif
             </dd>
+            <dt>&nbsp;</dt>
+            <dd>&nbsp;</dd>
+            <dt>&nbsp;</dt>
+            <dd>&nbsp;</dd>
         </dl>
-    </div>
 </div>
--- resources/views/issue/view/view.blade.php
+++ resources/views/issue/view/view.blade.php
@@ -1,18 +1,39 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-            @if($project != null)
-            <h5><a href="/project/{{$project->id}}/view" style="text-decoration: none"><span class="userName">{{$project->name}}</span>/<span class="userName">{{$project->key}}</span> - <span class="userName">{{$issue->id}}</span></a></h5>
-            <h3 class="text-left text-muted">{{$issue->summary}}
-            <a class="btn btn-default floatR" data-toggle="modal" data-target="#issueEdit">Edit</a>
-            </h3>
-            <hr>
-            @include('modal.issueEdit')
-            @include('modal.issueComment')
-            @include('modal.issueLog')
-            @include('modal.issueFile')
-            @include('issue.view.issueViewTemplates.leftViewPanel')
-            @include('issue.view.issueViewTemplates.rightViewPanel')
-        @endif
+        @if($project != null)
+        <h4>
+            <a href="/project/{{$project->id}}/view" style="text-decoration: none">
+                <span class="userName text-capitalize">{{$project->name}}</span>
+                /
+                <span class="userName">
+                    {{$project->key}}
+                </span> -
+                <span class="userName">
+                    {{$issue->id}}
+                </span>
+            </a>
+        </h4>
+        <hr>
+        <h3 class="text-left text-muted">{{$issue->summary}}
+        <a class="btn btn-default pull-right" data-toggle="modal" data-target="#issueEdit">Edit</a>
+        </h3>
+        <hr>
+        @include('modal.issueEdit')
+        @include('modal.issueComment')
+        @include('modal.issueLog')
+        @include('modal.issueFile')
+        <div class="row">
+            <div class="col-lg-6 col-sm-12 col-xs-12">
+                @include('issue.view.issueViewTemplates.leftViewPanel')
+            </div>
+            <div class="col-lg-6 col-sm-12 col-xs-12">
+                @include('issue.view.issueViewTemplates.rightViewPanel')
+            </div>
+            <div class="col-lg-6 col-sm-12 col-xs-12">
+                @include('issue.view.issueViewTemplates.activityTab')
+            </div>
+        </div>
+    @endif
     </div>
 @stop
